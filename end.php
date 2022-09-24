c321e017431085c1a6dbbd59645ca5ddc7214a0e
joshuakobuskie <102330744+joshuakobuskie@users.noreply.github.com>
2022-09-24 17:31:34 -0400

    alpha back end files

---
+++ end.php
@@ -0,0 +1,36 @@
+<?php
+//Accept "ID", "Password"
+//Query jsk47 user_accounts
+//Pass Json back with echo
+$servername = "sql1.njit.edu";
+$username = "jsk47";
+$password = "NJITHonors2022!";
+$dbname = "jsk47";
+$data = [];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = 'SELECT `Role` FROM user_accounts WHERE `ID` = "'.$_POST["ID"].'" AND
+  `Password` = "'.$_POST["Password"].'"';
+
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data
+      while($row = $result->fetch_assoc()) {
+          $data[] = "true";
+          $data[] = $row["Role"];
+            }
+    } else {
+      $data[] = "false";
+      $data[] = "Invalid Credentials!";
+    }
+    echo json_encode($data);
+    $conn->close();
+?>
